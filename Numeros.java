40b2a72c669249babc1f4ac77a10ca846469f9c3
Vinicius <oliveirateixeira653@gmail.com>
2024-11-01 14:54:48 -0300

    Add files via upload

---
+++ Numeros.java
@@ -0,0 +1,23 @@
+
+package Arrays ; 
+
+public class Numeros {
+    public static void main(String[] args) {
+        
+                    int[ ] numeros =  {10,20,30,0,50,60,70,80,90,100} ; 
+                    
+                    
+                    
+                    numeros[3] = 40 ;
+                    
+                    for (int num: numeros) {
+                        System.out.println(num) ; 
+                    }
+    }
+    
+    
+}
+
+
+
+// + < =  
---
+++ addElementos.java
@@ -0,0 +1,28 @@
+
+package Arrays ;
+
+public class addElementos {
+    
+    public static void main(String[] args) {
+        
+        int[] extencao  =  {} ; 
+       
+        
+        
+        int[] novoArray =  new int[extencao.length + 5] ; 
+        
+        novoArray[0] = 10 ; 
+        novoArray[1] = 20 ; 
+        novoArray[2] = 30 ;
+        novoArray[3] = 40 ; 
+        novoArray[4] = 50 ; 
+        
+                    System.out.println("Numeros...") ;
+        
+        for (int novo: novoArray) {
+            System.out.println(novo) ; 
+        }
+    }
+}
+
+// + < =  
